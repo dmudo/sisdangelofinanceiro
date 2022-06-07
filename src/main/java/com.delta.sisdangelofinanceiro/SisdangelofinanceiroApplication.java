c9fbf7077e46c37792e15f80f7096a24d5c24f04
D'angelo Martins <dangelomartins@gmail.com>
2022-06-06 22:48:41 -0300

    Create SisdangelofinanceiroApplication.java

---
+++ src/main/java/com.delta.sisdangelofinanceiro/SisdangelofinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.delta.sisdangelofinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisdangelofinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisdangelofinanceiroApplication.class, args);
+	}
+
+}
